5b8771a0004f6bd8ddeb0fb729dab468370ae7b9
Ojo Adebayo <bayoojo1@yahoo.com>
2020-05-26 18:31:23 +0100

    changed options name

--- pollsample.php
+++ pollsample.php
@@ -1,6 +1,6 @@
 <?php
 $apiToken = $apiToken = "905684105:AAEGtV8_rp-wjzpub40LucuaUJS4FLaQ_Uw";
-$options =  array("Yemi Osinbajo","Aba Kyari","Goodluck Jonathan", "Femi Gbajabiamila") ;
+$options =  array("Yemi Osinbajo","Aba Kyari","Goodluck Jonathan", "Gbajabiamila") ;
 
 $data = ['chat_id' => '@jobsarena',   'question' => 'What is the name of Nigeria Vice President ?',  'options' => json_encode($options), 'is_anonymous' => 'No' ];
 
